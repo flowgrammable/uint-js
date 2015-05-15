d5ee8f983ab5b1525708833b5ec6724c8e79c721
Jasson Casey <jasson@flowgrammable.com>
2015-05-14 22:51:31 -0400

    latest

--- tests/operations.js
+++ tests/operations.js
@@ -61,9 +61,13 @@ describe('Equality testing', function() {
   it('UInt({ value: 101, bytes: 4 }) === ...', function() {
     var uint1 = new uint.UInt({ value: 101, bytes: 4 });
     var uint2 = new uint.UInt({ value: 101, bytes: 4 });
+    var uint3 = new uint.UInt({ value: 100, bytes: 4 });
     expect(uint.equal(uint1, uint2)).to.be.true;
     expect(uint1.equal(uint2)).to.be.true;
     expect(uint2.equal(uint1)).to.be.true;
+
+    expect(uint1.notEqual(uint3)).to.be.true;
+    expect(uint.notEqual(uint2, uint3)).to.be.true;
   });
 });
 
@@ -71,10 +75,20 @@ describe('Relational testing', function() {
   it('UInt: [0,0,0,0,0,1] < [255, 255, 255, 255, 255, 255]', function(){
     var large = new uint.UInt({ value: [255, 255, 255, 255, 255, 255] });
     var small = new uint.UInt({ value: [0,0,0,0,0,1] });
+
     expect(small.less(small)).to.be.false;
     expect(small.less(large)).to.be.true;
     expect(large.less(small)).to.be.false;
     expect(large.less(large)).to.be.false;
+
+    expect(small.lessEqual(small)).to.be.true;
+    expect(large.lessEqual(large)).to.be.true;
+    
+    expect(small.greater(large)).to.be.false;
+    expect(large.greater(small)).to.be.true;
+
+    expect(small.greaterEqual(small)).to.be.true;
+    expect(large.greaterEqual(large)).to.be.true;
   });
 
 });
@@ -84,33 +98,33 @@ describe('Logical bitwise testing', function() {
 
 describe('Arithmetic testing', function() {
 
-  /* FIXME new - integrate
-  it('UInt addition ', function(){
-    var a = new UInt.UInt(null, [255, 255, 255, 255, 255, 254], 6);
-    var b = new UInt.UInt(null, [0,0,0,0,0,1], 6);
-    expect(a.or(b).value[5]).toBe(255);
+  /* FIXME uncomment once plus is implmented
+  it('UInt.plus', function(){
+    var a = new uint.UInt({ value: [255, 255, 255, 255, 255, 254], bytes: 6 });
+    var b = new uint.UInt({ value: [0,0,0,0,0,1], bytes: 6 });
+    expect(a.plus(b).value()[5]).to.equal(255);
   });
   */
 
-  /* FIXME new - integrate
-  it('UInt subtraction', function(){
-    var a = new UInt.UInt(null, [255, 255, 255, 255, 255, 0], 6);
-    var b = new UInt.UInt(null, [0,0,0,0,0,1], 6);
-    var ans = a.subt(b);
-    expect(ans.value[0]).toBe(255);
-    expect(ans.value[5]).toBe(255);
+  /* FIXME uncomment once minus is implemented
+  it('UInt.minus', function(){
+    var a = new uint.UInt({ value: [255, 255, 255, 255, 255, 0], bytes: 6 });
+    var b = new uint.UInt({ value: [0,0,0,0,0,1], bytes: 6 });
+    var ans = a.minus(b);
+    expect(ans.value()[0]).to.equal(255);
+    expect(ans.value()[5]).to.equal(255);
     
-    var c = new UInt.UInt(null, [255, 0, 0, 0, 0, 1], 6);
-    var d = new UInt.UInt(null, [255, 0, 0, 0, 0, 0], 6);
-    var ansb = c.subt(d);
-    expect(ansb.value[0]).toBe(0);
-    expect(ansb.value[5]).toBe(1);
+    var c = new uint.UInt({ value: [255, 0, 0, 0, 0, 1], bytes: 6 });
+    var d = new uint.UInt({ value: [255, 0, 0, 0, 0, 0], bytes: 6 });
+    var ansb = c.minus(d);
+    expect(ansb.value()[0]).to.equal(0);
+    expect(ansb.value()[5]).to.equal(1);
     
-    var e = new UInt.UInt(null, [255, 0, 0, 0, 0, 0], 6);
-    var f = new UInt.UInt(null, [0, 0, 0, 0, 0, 1], 6);
-    var ansc = e.subt(f);
-    expect(ansc.value[0]).toBe(254);
-    expect(ansc.value[5]).toBe(255);
+    var e = new uint.UInt({ value: [255, 0, 0, 0, 0, 0], bytes: 6 });
+    var f = new uint.UInt({ value: [0, 0, 0, 0, 0, 1], bytes: 6 });
+    var ansc = e.minus(f);
+    expect(ansc.value()[0]).to.equal(254);
+    expect(ansc.value()[5]).to.equal(255);
   });
   */
 
